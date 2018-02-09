3140ec771e8adcced720976a3237c02baf4f3249
juan serrano <germgerm@gmail.com>
2018-02-08 23:57:24 -0800

    Add project info.  Add day 25

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-advent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure advent "
+  :url "https://github.com/germ13/clojure-advent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
