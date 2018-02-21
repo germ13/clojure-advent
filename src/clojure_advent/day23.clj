019d1ced8abe9b9445649574d9accaeebe60062d
juan serrano <germgerm@gmail.com>
2018-02-21 00:45:39 -0800

    Set up movement.

--- src/clojure_advent/day23.clj
+++ src/clojure_advent/day23.clj
@@ -1,36 +1,40 @@
-(def direction {
-    :right [1 0]
-    :up [0 1]
-    :left [-1 0]
-    :down [0 -1]
-})
+(ns day23)
 
-(def k 1)
+(def direction [
+    [  1  0 ]
+    [  0  1 ]
+    [ -1  0 ]
+    [  0 -1 ]
+])
 
-(def endpoint 15)
+(direction 1)
 
+
+
+
+
+(defn move [location heading]
+      [ (+ (first location) (first (nth direction (mod heading 4))))
+        (+ (second location) (second (nth direction (mod heading 4))))])
+
+
+(move [3 3] 0)
+(move [3 3] 1)
+(move [3 3] 2)
+(move [3 3] 3)
+
+
+(def endpoint 14)
 (loop [
         counter 1
         current-coordinates [0 0]
-        movement (:right direction)
-        scalar 1
+        movement 0
+        scalar 2
         increase-scalar 0]
   (if (= counter endpoint)
     current-coordinates
-    recur ((inc counter)
+    (recur (inc counter)
            (move current-coordinates movement)
-           (mod) 
-    
-  
-(defn move [location heading]
-      [ (+ (first location) (first (direction heading))) 
-        (+ (second location) (second (direction heading)))])
-  
-
-  
-(move [3 3] :up)
-(move [3 3] :up)
-(move [3 3] :right)
-(move [3 3] :left)
-
-
+           (if (= increase-scalar scalar) (inc movement) movement)
+           (if (= increase-scalar scalar) (inc scalar) scalar)
+           (mod (inc increase-scalar) scalar))))
