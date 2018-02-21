eea55c5bc6cd4a37b141cfafc1526ad9642c409a
juan serrano <germgerm@gmail.com>
2018-02-20 23:58:28 -0800

    Day 23 logic

--- src/clojure_advent/day23.clj
+++ src/clojure_advent/day23.clj
@@ -1,9 +1,36 @@
-(def direction [:right :up :left :down])
-
-(def direction-delta (
+(def direction {
     :right [1 0]
     :up [0 1]
     :left [-1 0]
     :down [0 -1]
-))
+})
+
+(def k 1)
+
+(def endpoint 15)
+
+(loop [
+        counter 1
+        current-coordinates [0 0]
+        movement (:right direction)
+        scalar 1
+        increase-scalar 0]
+  (if (= counter endpoint)
+    current-coordinates
+    recur ((inc counter)
+           (move current-coordinates movement)
+           (mod) 
+    
+  
+(defn move [location heading]
+      [ (+ (first location) (first (direction heading))) 
+        (+ (second location) (second (direction heading)))])
+  
+
+  
+(move [3 3] :up)
+(move [3 3] :up)
+(move [3 3] :right)
+(move [3 3] :left)
+
 
